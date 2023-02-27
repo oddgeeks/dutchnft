52e2776cfdf67faf0ea46d99988e5ac2a102867a
oddgeeks <oddgeeks.dev@gmail.com>
2023-02-27 16:12:16 +0100

    feat: add satoshi font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-satoshi font-bold underline">Hello world!</h1>
+  );
 }
