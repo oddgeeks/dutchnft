161b8f0a5e2b75f450ccff38386d8ab60285c914
oddgeeks <oddgeeks.dev@gmail.com>
2023-02-28 13:30:25 +0100

    config: enable styled-components SWC transform

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
   async redirects() {
     return [
       {
