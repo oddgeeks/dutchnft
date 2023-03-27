c17d18bc07de50a14e8e7ae5a6bc23dd94a8887f
oddgeeks <oddgeeks.dev@gmail.com>
2023-03-27 18:44:40 +0530

    feat: added lottie animation in navbar

--- src/components/dashboard/SubNav.tsx
+++ src/components/dashboard/SubNav.tsx
@@ -4,32 +4,50 @@ import { useRouter } from 'next/router';
 // components
 import { Link } from '@/common';
 import * as DutchC from './styles';
+import LottieAnimationPlayer from './lottie-animation-player';
+
+// Animations Json
+import NFT from '@/assets/lottie-animation/NFT.json';
+import FindHolders from '@/assets/lottie-animation/Find_Holders.json';
+import AirDrop from '@/assets/lottie-animation/Air_Drop.json';
+import TradeIn from '@/assets/lottie-animation/Trade_In.json';
+import Analytics from '@/assets/lottie-animation/Analytics.json';
 
 const subnavs = [
   {
     name: 'NFT Management',
     path: '/dashboard/nft-management',
     slug: 'nft-management',
+    animationData: NFT,
+    bgColor: 'rgba(46, 198, 46, 0.2)',
   },
   {
     name: 'Find Holders',
     path: '/dashboard/holders',
     slug: 'holders',
+    animationData: FindHolders,
+    bgColor: 'rgba(245, 215, 46, 0.2)',
   },
   {
     name: 'Airdrop',
     path: '/dashboard/airdrop',
     slug: 'airdrop',
+    animationData: AirDrop,
+    bgColor: 'rgba(46, 196, 218, 0.2)',
   },
   {
     name: 'Trade-in',
     path: '/dashboard/trade-in',
     slug: 'trade-in',
+    animationData: TradeIn,
+    bgColor: 'rgba(167, 76, 201, 0.2)',
   },
   {
     name: 'Sales',
     path: '/dashboard/sales',
     slug: 'sales',
+    animationData: Analytics,
+    bgColor: 'rgba(255, 79, 182, 0.2)',
   },
 ];
 
@@ -40,14 +58,19 @@ const DashboardSubNav: React.FC = () => {
 
   return (
     <DutchC.SubNavWrapper>
-      {subnavs.map((nav) => (
+      {subnavs.map((nav, index) => (
         <Link
           key={nav.slug}
           href={nav.path}
           active={PAGE_PATH === nav.slug ? 1 : 0}
           size="small"
         >
-          {nav.name}
+          <LottieAnimationPlayer
+            animationData={nav.animationData}
+            bgColor={nav.bgColor}
+            name={nav.name}
+            index={index}
+          />  
         </Link>
       ))}
     </DutchC.SubNavWrapper>
---
+++ src/components/dashboard/lottie-animation-player/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import Lottie from 'lottie-react';
+
+// types
+type LottieWrapperProps = {
+  bgColor?: string;
+};
+
+// components
+export const LottieWrapper = styled.div.attrs({
+  className: 'flex rounded-lg mb-2',
+})`
+  background-color: ${(p: LottieWrapperProps) => p.bgColor};
+`;
+
+export const LottieAnimationWrapper = styled(Lottie).attrs({
+  className: 'flex',
+})``;
+
+export const TextWrapper = styled.span.attrs({
+  className: 'font-bold font-Satoshi text-base text-black'
+})``;
