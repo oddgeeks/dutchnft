f92f8395db5cf5e6fd15a788efdbd0394f4250ad
oddgeeks <oddgeeks.dev@gmail.com>
2023-04-16 21:18:05 -0700

    fix: applayout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,7 +34,6 @@ export default function App({ Component, pageProps }: AppProps) {
           <ApolloProvider client={apolloConfig}>
             <Component {...pageProps} />
           </ApolloProvider>
-          ,
           <ToastContainer />
         </ThemeProvider>
       </PersistGate>
