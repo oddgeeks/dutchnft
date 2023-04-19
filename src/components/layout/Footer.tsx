d81b8bdb79382dfa1ef085654f6e8e81d8dfadc4
oddgeeks <oddgeeks.dev@gmail.com>
2023-04-19 12:14:47 +0200

    fix: Create Footer and add in every page #99

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useTheme } from 'next-themes';
+import DiscordIcon from '../../assets/social/discord_solid.svg';
+import TwitterIcon from '../../assets/social/twitter_solid.svg';
+import InstagramIcon from '../../assets/social/instagram_solid.svg';
+import LinkedinIcon from '../../assets/social/linkedin_solid.svg';
+
+const Footer = () => {
+  const { theme } = useTheme();
+  return (
+    <div className="absolute bottom-0 left-0 w-full flex justify-between px-6 py-3 z-50 border border-black/10 dark:border-white/10">
+      <div className="text-sm">© Rarible, Inc.</div>
+      <div className="flex gap-6 items-center">
+        <DiscordIcon currentColor={theme === 'dark' ? 'white' : 'black'} />
+        <InstagramIcon currentColor={theme === 'dark' ? 'white' : 'black'} />
+        <TwitterIcon currentColor={theme === 'dark' ? 'white' : 'black'} />
+        <LinkedinIcon currentColor={theme === 'dark' ? 'white' : 'black'} />
+      </div>
+      <div className="flex gap-8">
+        <p>Terms</p>
+        <p>Privacy policy</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
